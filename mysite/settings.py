ded229f04b810115fe66ac84b8e29e169c915c78
Kuystad <kuystad.r@gmail.com>
2023-12-15 21:49:38 +0300

    l_19

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'django',
+       'USER': 'django',
+       'PASSWORD': 'django',
+       'HOST': 'localhost',
+       'PORT': 5432,
+   }
 }
+
 
 
 # Password validation
